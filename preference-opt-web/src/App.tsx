05dcdcbdd2e7402569806c1073cbf533288547e7
Renato <renato145@hotmail.com>
2021-03-17 01:14:15 +1030

    wasm import working

--- preference-opt-web/src/App.tsx
+++ preference-opt-web/src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
+import { OptimizationEngine } from "preference-opt-wasm";
 
 export const App = () => {
+  const samples = new Float64Array([1, 0, 2, 4]);
+  const preferences = new Uint32Array([0, 1]);
+  const engine = OptimizationEngine.new(samples, preferences, 2);
+  const dims = engine.dims();
+
   return (
     <div className="bg-blue-200 p-20">
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <p className="p-5 text-red-900">Dims: {dims}</p>
     </div>
   );
 };
--- preference-opt-web/src/index.tsx
+++ preference-opt-web/src/index.tsx
@@ -1,17 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import { App } from "./App";
-import "./index.css";
+// A dependency graph that contains any wasm must all be imported
+// asynchronously. This `index.js` file does the single async import, so
+// that no one else needs to worry about it again.
+import("./setup")
+  .catch(e => console.error("Error importing `setup.tsx`:", e));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Need to make this file a module
+export {};
