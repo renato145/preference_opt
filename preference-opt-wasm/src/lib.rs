1a52f5a2be2371c0335485b25cf6aac99a986f5c
Renato <renato145@hotmail.com>
2021-03-16 23:34:06 +1030

    first wasm compile

--- preference-opt-wasm/src/lib.rs
+++ preference-opt-wasm/src/lib.rs
@@ -1,5 +1,3 @@
-mod utils;
-
 use preference_opt::PreferenceOpt;
 use wasm_bindgen::prelude::*;
 
@@ -16,26 +14,54 @@ pub struct OptimizationEngine {
 
 #[wasm_bindgen]
 impl OptimizationEngine {
-    pub fn new(// samples: Vec<Vec<f64>>,
-        // preferences: Vec<(usize, usize)>,
+    pub fn new(
+        samples: Vec<f64>,
+        preferences: Vec<usize>,
+        dimensions: usize,
     ) -> OptimizationEngine {
-        let samples = vec![vec![1.0, 0.0], vec![2.0, 5.0]];
-        let preferences = vec![(1, 0)];
+        let samples = samples
+            .chunks(dimensions)
+            .map(|o| o.to_vec())
+            .collect::<Vec<_>>();
+        let preferences = preferences
+            .chunks(2)
+            .map(|o| (o[0], o[1]))
+            .collect::<Vec<_>>();
         let engine = PreferenceOpt::from_data(samples, preferences).unwrap();
         Self { engine }
     }
 
-    pub fn tt(&self) -> f64 {
-        self.engine.alpha
+    pub fn with_bounds(mut self, bounds: Vec<f64>) -> Self {
+        let bounds = bounds.chunks(2).map(|o| (o[0], o[1])).collect::<Vec<_>>();
+        self.engine = self.engine.with_bounds(bounds).unwrap();
+        self
+    }
+
+    pub fn with_same_bounds(mut self, bounds: Vec<f64>) -> Self {
+        let bounds = (bounds[0], bounds[1]);
+        self.engine = self.engine.with_same_bounds(bounds).unwrap();
+        self
+    }
+
+    pub fn dims(&self) -> usize {
+        self.engine.dims()
     }
 }
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, preference-opt-wasm!");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_engine() {
+        let opt = OptimizationEngine::new(vec![1.0, 0.0, 2.0, 5.0], vec![1, 0], 2)
+            .with_same_bounds(vec![0.0, 10.0]);
+        opt.engine.x.show();
+        opt.engine.m.show();
+    }
 }
--- preference-opt-wasm/tests/web.rs
+++ preference-opt-wasm/tests/web.rs
@@ -2,18 +2,13 @@
 
 #![cfg(target_arch = "wasm32")]
 
-use wasm_bindgen_test::*;
 use preference_opt_wasm::OptimizationEngine;
+use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
-}
-
-#[wasm_bindgen_test]
 fn get_engine() {
-    let opt = OptimizationEngine::new();
-    assert_eq!(opt.tt(), 1e-5);
+    let opt = OptimizationEngine::new(vec![1.0, 0.0, 2.0, 5.0], vec![1, 0], 2);
+    assert_eq!(opt.dims(), 2);
 }
