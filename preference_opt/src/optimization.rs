3958d89051a96b576fa6d56fd0fd72da72fc3511
Renato <renato145@hotmail.com>
2021-03-16 23:38:01 +1030

    empty test

--- preference_opt/src/optimization.rs
+++ preference_opt/src/optimization.rs
@@ -446,4 +446,19 @@ mod tests {
         opt.m.show();
         Ok(())
     }
+
+    // #[test]
+    // fn optimize_fn_empty_test() -> Result<()> {
+    //     let samples = vec![vec![1.0, 0.0], vec![2.0, 5.0]];
+    //     let preferences = vec![];
+    //     let mut opt =
+    //         PreferenceOpt::from_data(samples, preferences)?.with_same_bounds((0.0, 10.0))?;
+    //     let func = |o: &[f64]| o.iter().sum();
+    //     let (optimal_values, f_posterior) = opt.optimize_fn(func, 1, None, 1, 1)?;
+    //     println!("optimal_values -> {}", optimal_values);
+    //     println!("f_posterior -> {}", f_posterior);
+    //     opt.x.show();
+    //     opt.m.show();
+    //     Ok(())
+    // }
 }
