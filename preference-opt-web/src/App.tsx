4e005a57099f5b60cdfb93c51143a71c812cdc88
Renato <renato145@hotmail.com>
2021-03-17 00:15:02 +1030

    initial web

---
+++ preference-opt-web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="bg-blue-200 p-20">
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
