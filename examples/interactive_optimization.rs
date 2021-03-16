20f91fb9e3c486d3ac071fce575bc8ff81ccfe1e
Renato <renato145@hotmail.com>
2021-03-16 17:51:48 +1030

    modify example

--- examples/interactive_optimization.rs
+++ examples/interactive_optimization.rs
@@ -2,14 +2,14 @@ use anyhow::Result;
 use preference_opt::*;
 
 fn main() -> Result<()> {
-    let samples = vec![vec![1.0, 0.0], vec![2.0, 5.0]];
+    let samples = vec![vec![1.0, 0.0], vec![0.0, 2.0]];
     let preferences = vec![(1, 0)];
-    let mut opt = PreferenceOpt::from_data(samples, preferences)?.with_same_bounds((0.0, 50.0))?;
+    let mut opt = PreferenceOpt::from_data(samples, preferences)?.with_same_bounds((0.0, 10.0))?;
     println!("Try to choose the examples with the higher values...");
-    let (optimal_values, f_posterior) = opt.interactive_optimization(100, None, 1, 1)?;
+    let (optimal_values, _f_posterior) = opt.interactive_optimization(100, None, 500, 1)?;
     println!("optimal_values -> {}", optimal_values);
-    println!("f_posterior -> {}", f_posterior);
-    opt.x.show();
-    opt.m.show();
+    // println!("f_posterior -> {}", f_posterior);
+    // opt.x.show();
+    // opt.m.show();
     Ok(())
 }
